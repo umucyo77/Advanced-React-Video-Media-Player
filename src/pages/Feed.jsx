e735e6325159f1ee83a7ee1d7c40b7a0b296e602
umucyo77 <eumucyuvakumana77@gmail.com>
2026-03-14 19:36:32 +0200

    Feed Page (API Fetching with React Query)

---
+++ src/pages/Feed.jsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import VideoCard from "../components/VideoCard";
+import Loader from "../components/Loader";
+
+function Feed() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["videos"],
+    queryFn: () => fetchFromAPI("/search?part=snippet&q=react&type=video"),
+  });
+
+  if (isLoading) return <Loader />;
+  if (isError) return <h2>Error loading videos</h2>;
+
+  return (
+    <div className="video-grid">
+      {data.items.map((video) => (
+        <VideoCard key={video.id.videoId} video={video} />
+      ))}
+    </div>
+  );
+}
+
+export default Feed;
